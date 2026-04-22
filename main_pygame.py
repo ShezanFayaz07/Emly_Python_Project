f7a4de64b2085fa3509b20899565fa450855250b
Shezan Fayaz <shezanf7@gmail.com>
2026-04-22 21:08:25 +0530

    Refactor game logic

--- main_pygame.py
+++ main_pygame.py
@@ -1,6 +1,11 @@
 import pygame
 pygame.init()
 
+
+from pygame_logic import displayWordProgress, getUserGuess, initializeGameState, processGuess
+
+
+
 WIN =  pygame.display.set_mode((1200, 720))
 pygame.display.set_caption("Hangman Game")
 
@@ -20,9 +25,12 @@ guessed_letters = set()
 WordsList = ["apple", "banana", "grapes", "orange", "mango"]
 
 selected_word = ''
-User_Guess = ''
+user_guess = ''
+initialize_message = ''
+validation_message = ''
+message_processedGuess = ''
 
-Wrong_letters = set()
+wrong_letters = set()
 
 def draw_menu():
     WIN.fill((30, 30, 30))
@@ -55,6 +63,21 @@ def draw_rules():
         WIN.blit(rule_text, (1200//2 - rule_text.get_width()//2, 150 + rules.index(i) * 50 ))
 
 
+
+def draw_game():
+    WIN.fill((30, 30, 30))
+    displayWordProgress(WIN, selected_word, guessed_letters, attempts_left)
+
+    display_initialization_text = font_small.render(initialize_message, True, (255, 255, 255))
+    WIN.blit(display_initialization_text, (1200//2 - display_initialization_text.get_width()//2, 200))
+
+    validation_text = font_small.render(validation_message, True, (255, 0, 0))
+    WIN.blit(validation_text, (1200//2 - validation_text.get_width()//2, 300))
+
+    processedGuess_text = font_small.render(message_processedGuess, True, (0, 255, 0))
+    WIN.blit(processedGuess_text, (1200//2 - processedGuess_text.get_width()//2, 400))
+
+
 running = True
 while running:
     clock.tick(60)
@@ -70,9 +93,31 @@ while running:
                 elif event.key == pygame.K_n:
                     running = False
 
+
+        elif game_state == "rules":
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RETURN:
+                    selected_word, attempts_left, initialize_message = initializeGameState(selected_word, WordsList , attempts_left)
+                    game_state = "game"
+                elif event.key == pygame.K_n:
+                    running = False
+
+        elif game_state == "game":
+            if event.type == pygame.KEYDOWN:
+                guess = pygame.key.name(event.key)
+                print(f"User Guess: {guess}")
+                guess , validation_message = getUserGuess(WIN, guessed_letters, wrong_letters , guess)
+                print(f"Processed Guess: {guess}")
+                if guess == "z":
+                    running = False
+                if guess != False:
+                    guessed_letters, wrong_letters, attempts_left , message_processedGuess = processGuess(WIN, guess, selected_word, guessed_letters, wrong_letters, attempts_left)
+
     if game_state == "menu":
         draw_menu()
     elif game_state == "rules":
         draw_rules()
+    if game_state == "game":
+        draw_game()
     pygame.display.update()
 pygame.quit()
